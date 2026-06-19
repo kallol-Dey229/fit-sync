0be7e77cc4be1a14ab752d73c2286aeee992454e
Kallol Dey <kalloldey067@gmail.com>
2026-06-20 00:04:16 +0600

    conditional navbar and signout done

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+
+
+const UserDashBoard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UserDashBoard;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Flame } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button, Dropdown } from "@heroui/react";
 
 export default function Navbar() {
   const pathname = usePathname();
@@ -13,11 +15,20 @@ export default function Navbar() {
     { name: "Community Forum", href: "/community-forum" },
   ];
 
+  const { data: session } = authClient.useSession();
+  const user = session?.user;
+
+  const handleSignOut = async () => {
+
+    await authClient.signOut();
+
+  }
+
   return (
     <nav className="sticky top-0 z-50 backdrop-blur-md border-b border-white/10 bg-[#070b17]/95">
       <div className="max-w-7xl mx-auto px-6">
         <div className="h-20 flex items-center justify-between">
-          
+
           {/* Logo */}
           <Link href="/" className="flex items-center gap-3">
             <div className="w-11 h-11 rounded-lg bg-[#ff5a1f] flex items-center justify-center">
@@ -36,11 +47,10 @@ export default function Navbar() {
               <Link
                 key={item.href}
                 href={item.href}
-                className={`font-semibold transition-all duration-300 ${
-                  pathname === item.href
-                    ? "text-[#ff5a1f]"
-                    : "text-[#8b8ca7] hover:text-white"
-                }`}
+                className={`font-semibold transition-all duration-300 ${pathname === item.href
+                  ? "text-[#ff5a1f]"
+                  : "text-[#8b8ca7] hover:text-white"
+                  }`}
               >
                 {item.name}
               </Link>
@@ -49,19 +59,68 @@ export default function Navbar() {
 
           {/* Actions */}
           <div className="flex items-center gap-5">
-            <Link
-              href="/auth/signin"
-              className="text-[#8b8ca7] hover:text-white transition"
-            >
-              Sign In
-            </Link>
 
-            <Link
-              href="/auth/signup"
-              className="px-6 py-3 rounded-lg bg-[#ff5a1f] hover:bg-[#ff6b35] text-white font-semibold transition-all duration-300"
-            >
-              Join Now
-            </Link>
+            {
+              user ? <>
+                <p className="font-bold text-blue-800">Hi, {user.name}</p>
+
+                <Link href="/dashboard">
+                <Button variant="secondary" className={"text-gray-400 rounded-lg"}>Dashboard</Button>
+                </Link>
+
+
+<Dropdown>
+
+            <Button className="rounded-full w-10 h-10" aria-label="Menu" variant="outline">
+                <Avatar>
+                    <Avatar.Image referrerPolicy="no-referrer" alt="user image" src={user?.image} />
+                    <Avatar.Fallback>{user?.name?.charAt(0)}</Avatar.Fallback>
+                </Avatar>
+            </Button>
+
+            <Dropdown.Popover className={'rounded-md'}>
+
+                <Dropdown.Menu>
+
+                    <Dropdown.Item id="profile" textValue="Profile">
+                        <Link href="/profile" className="block w-full">
+                            Profile
+                        </Link>
+                    </Dropdown.Item>
+
+
+                </Dropdown.Menu>
+
+            </Dropdown.Popover>
+
+        </Dropdown>
+
+
+
+
+
+
+
+                <Button onClick={handleSignOut} variant="danger" className={"rounded-lg"}>Sign Out</Button>
+              </> :
+                <>
+                  <Link
+                    href="/auth/signin"
+                    className="text-[#8b8ca7] hover:text-white transition"
+                  >
+                    Sign In
+                  </Link>
+
+                  <Link
+                    href="/auth/signup"
+                    className="px-6 py-3 rounded-lg bg-[#ff5a1f] hover:bg-[#ff6b35] text-white font-semibold transition-all duration-300"
+                  >
+                    Join Now
+                  </Link>
+                </>
+            }
+
+
           </div>
         </div>
       </div>
