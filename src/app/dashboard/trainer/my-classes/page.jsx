e4fc2218ca3369bc6c842fb0e354e1f3b6671faa
Kallol Dey <kalloldey067@gmail.com>
2026-09-21 21:54:27 +0600

    trainer my classes button fixed

--- src/app/dashboard/trainer/my-classes/page.jsx
+++ src/app/dashboard/trainer/my-classes/page.jsx
@@ -15,7 +15,7 @@ const MyClassesPage = async () => {
     
   return (
     <div className="min-h-screen bg-[#0B0C10] text-white p-8">
-      {/* Title */}
+      
       <h1 className="text-2xl font-black uppercase tracking-wider mb-8">
         My Classes
       </h1>
